899ee4844461677048def3bf5e5e0f9478f4a273
Divyansh895 <77732132+Divyansh895@users.noreply.github.com>
2021-07-20 19:11:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas=createCanvas(400,400);
+    canvas.center();
+}
+
+
+
+function draw() {
+    
+}
+
+function take_snapsot() {
+    save('mustache.png');
+}
+
